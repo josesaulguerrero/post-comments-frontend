113d4c782430633fd0b3f96334712ed436b5ab65
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-09-05 10:24:56 -0500

    Add handler on CommentForm controller to handle input fields

--- src/app/forms/comment-form/comment-form.component.ts
+++ src/app/forms/comment-form/comment-form.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { CreateComment } from 'src/app/models/commands/CreateComment';
 
 @Component({
   selector: 'app-comment-form',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./comment-form.component.css'],
 })
 export class CommentFormComponent {
-  constructor() {}
+  public author!: String;
+  public content!: String;
+
+  constructor(private httpClient: HttpClient) {
+    this.author = '';
+    this.content = '';
+  }
+
+  public handleSubmit() {
+    const command: CreateComment = {
+      author: this.author,
+      content: this.content,
+      postedAt: new Date(),
+    };
+
+    this.httpClient
+      .post<unknown>('url', command, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .subscribe(console.log);
+  }
 }
