187f73e33a930890ff907bcac680f715f32fc023
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-09-05 10:09:37 -0500

    Add handler on PostForm controller to handle input fields

--- src/app/forms/post-form/post-form.component.ts
+++ src/app/forms/post-form/post-form.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { CreatePost } from './../../models/commands/CreatePost';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-post-form',
   templateUrl: './post-form.component.html',
-  styleUrls: ['./post-form.component.css']
+  styleUrls: ['./post-form.component.css'],
 })
-export class PostFormComponent implements OnInit {
+export class PostFormComponent {
+  public author!: String;
+  public title!: String;
+  public content!: String;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private httpClient: HttpClient) {
+    this.author = '';
+    this.title = '';
+    this.content = '';
   }
 
+  public handleSubmit() {
+    const command: CreatePost = {
+      author: this.author,
+      content: this.content,
+      title: this.title,
+      postedAt: new Date(),
+    };
+
+    this.httpClient
+      .post<unknown>('url', command, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .subscribe(console.log);
+  }
 }
