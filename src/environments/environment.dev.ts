188c920056fe5311a47def46857e643cdc4dff7f
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-09-08 11:34:55 -0500

    Fix typo on environment gamma url

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   ALPHA_URL: 'http://localhost:8080',
   BETA_URL: 'http://localhost:8090',
-  GAMMA_URL: 'ws://localhost:8090',
+  GAMMA_URL: 'ws://localhost:8070',
 };
