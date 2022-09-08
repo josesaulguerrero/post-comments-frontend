83f5e552adbb05a76aaa324c07bc1fe4b132ce68
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-09-08 12:17:24 -0500

    Fix the html template

--- src/app/forms/comment-form/comment-form.component.ts
+++ src/app/forms/comment-form/comment-form.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CreateComment } from 'src/app/models/commands/CreateComment';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-comment-form',
@@ -8,27 +9,31 @@ import { CreateComment } from 'src/app/models/commands/CreateComment';
   styleUrls: ['./comment-form.component.css'],
 })
 export class CommentFormComponent {
-  public author!: String;
-  public content!: String;
+  @Input()
+  public postId!: string;
+  public author!: string;
+  public content!: string;
+  private baseURL: string;
 
   constructor(private httpClient: HttpClient) {
     this.author = '';
     this.content = '';
+    this.baseURL = `${environment.ALPHA_URL}/add/comment`;
   }
 
   public handleSubmit() {
     const command: CreateComment = {
+      postId: this.postId,
       author: this.author,
       content: this.content,
       postedAt: new Date(),
     };
-
     this.httpClient
-      .post<unknown>('url', command, {
+      .post<unknown>(this.baseURL, command, {
         headers: {
           'Content-Type': 'application/json',
         },
       })
-      .subscribe(console.log);
+      .subscribe();
   }
 }
