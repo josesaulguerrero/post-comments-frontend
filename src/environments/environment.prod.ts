622fa9b84e56e3672cf842b730c4e7ca441eb134
Jose Saúl Serrano <josesaulserrano15@gmail.com>
2022-09-14 09:41:56 -0500

    Set up firebase deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   ALPHA_URL: 'https://murmuring-eyrie-57231.herokuapp.com',
   BETA_URL: 'https://sheltered-headland-06914.herokuapp.com',
-  GAMMA_URL: 'https://young-ridge-20094.herokuapp.com',
+  GAMMA_URL: 'WSS://young-ridge-20094.herokuapp.com',
 };
