acf5af28daa8d42d02ace0d514580e455cd22de8
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-09-15 13:39:27 -0500

    Set up firebase env variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
   firebase: {
-    projectId: 'post-comments-c72b8',
-    appId: '1:169596359202:web:8bfedbe3973b045dea707b',
-    storageBucket: 'post-comments-c72b8.appspot.com',
-    apiKey: 'AIzaSyAGoke5rbMXbjqrnFCsiA7Bt2Kjw58DmqU',
-    authDomain: 'post-comments-c72b8.firebaseapp.com',
-    messagingSenderId: '169596359202',
-    measurementId: 'G-V94RLED3CC',
+    projectId: 'project id',
+    appId: 'app id',
+    storageBucket: 'storage bucket',
+    apiKey: 'api key',
+    authDomain: 'domain',
+    messagingSenderId: 'sender id',
+    measurementId: 'measurement id',
   },
   production: false,
   ALPHA_URL: 'url',
